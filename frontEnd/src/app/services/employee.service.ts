b86108da626a1c1be19d89f08a83fe372c72ac39
unknown <fabian.arancibia93@gmail.com>
2019-04-27 00:54:51 -0400

    se crea proyecto Angular, y se añaden componentes y funcionalidad

---
+++ frontEnd/src/app/services/employee.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import{ HttpClient } from '@angular/common/http';
+import { Employee } from '../models/employee';
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  selectedEmployeed: Employee;
+  employees: Employee[];
+  readonly URL_API : 'http://localhost:3000/api/employee';
+
+  constructor(private http:HttpClient) { }
+
+  getEmployees(){
+    return this.http.get(this.URL_API+'/');
+  }
+  postEmployees(employee:Employee){
+    return this.http.post(this.URL_API+'/',employee);
+  }
+  putEmployees(employee:Employee){
+    return this.http.put(this.URL_API+'/'+`$employee._id`,employee);
+  }
+  deleteEmployees(_id:string){
+    return this.http.delete(this.URL_API+`/$_id`);
+  }
+}
